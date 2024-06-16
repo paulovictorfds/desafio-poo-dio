9aa8a03d39337828cb34003518ee320f5f584efa
Paulo Victor Souza <p.victordsouza@gmail.com>
2024-06-16 02:45:34 -0300

    finalizando projeto

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,32 +2,32 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria extends Conteudo{
+public class Mentoria extends Conteudo {
 
-    private LocalDate data;
+  private LocalDate data;
 
-    @Override
-    public double calcularXp() {
-        return XP_PADRAO + 20d;
-    }
+  @Override
+  public double calcularXp() {
+    return XP_PADRAO + 20d;
+  }
 
-    public Mentoria() {
-    }
+  public Mentoria() {
+  }
 
-    public LocalDate getData() {
-        return data;
-    }
+  public LocalDate getData() {
+    return data;
+  }
 
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
 
-    @Override
-    public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Mentoria{" +
+        "titulo='" + getTitulo() + '\'' +
+        ", descricao='" + getDescricao() + '\'' +
+        ", data=" + getData() +
+        '}';
+  }
 }
